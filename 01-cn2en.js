9f485fd886a1b0c64e6433da3bf11c5187b44b1a
pogusanqian <pogusanqian@163.com>
2021-08-26 12:25:17 +0800

    直接复制到剪切板

--- 01-cn2en.js
+++ 01-cn2en.js
@@ -1,10 +1,12 @@
 // 中文标点符号映射成英文
 const fs = require('fs');
 const minimist = require('minimist');
+const { exec } = require('child_process');
+const iconv = require('iconv-lite');
 
 const argv = minimist(process.argv.slice(2), {
   default: {
-    path: 'C:\\Users\\pogus\\Documents\\GreenSoft\\todyStudy.txt'
+    path: 'D:/GWare/TodyStudy.txt'
   }
 })
 
@@ -22,5 +24,8 @@ const path = argv.path;
 
 let result = fs.readFileSync(path, 'utf8');
 replaceConfigMap.forEach((value, key) => result = result.replace(new RegExp(key, 'g'), value));
+
+// 输出到控制台, 并更改文件, 并复制到剪切板上, 注意复制到剪切板的字符编码一定要是GBK编码
 console.log(result);
 fs.writeFileSync(path, result);
+exec('clip').stdin.end(iconv.encode(result, 'gbk'));
